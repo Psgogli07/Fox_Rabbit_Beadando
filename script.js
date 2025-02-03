d8bd8375c60c0ce17a60c77eee2a83054622726f
Kis Levente <kis.levente@ckik.loc>
2025-02-03 13:17:52 +0100

    roka

--- script.js
+++ script.js
@@ -4,9 +4,16 @@ const table = document.querySelector("table");
 const fox = { x: n-1, y: m-1 }; 
 const rabbit = { x: n-1, y: m-1 };
 
+function randint(a, b) {
+    return Math.floor(Math.random() * (b-a+1)) + a;
+}
+
 function start(){
     showBoard()
+    poziciok()
     button.disabled = true;  
+    fox = { x: n, y: m }; 
+    rabbit = { x: n-1, y: m-1 };
 };
 
 function showBoard() {
@@ -15,12 +22,17 @@ function showBoard() {
         const tr = document.createElement("tr");
         for (let j = 0; j < m; j++) {
             const td = document.createElement("td");
-            td.innerText = "a";
+            td.innerText = " ";
             tr.appendChild(td);
         }
         table.appendChild(tr);
     }
+    
 }
+
+function poziciok(){
+    table.rows[4].cells[11].innerText = "🦊";
+}   
 
 const button = document.querySelector("button")
 button.addEventListener("click", start)
