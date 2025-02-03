620d786c21e55783375cde5b8c747cf6030b906d
Kis Levente <kis.levente@ckik.loc>
2025-02-03 10:31:55 +0100

    asd

--- script.js
+++ script.js
@@ -1,22 +1,20 @@
-
-const n = 12;
-const m = 5;
+const n = 5;
+const m = 12;
 const table = document.querySelector("table");
 const fox = { x: n-1, y: m-1 }; 
 const rabbit = { x: n-1, y: m-1 };
-
-function start(){
-    showBoard
-    button.disabled = true;  
+function startGame(){
+    showBoard();
+    //button.disabled = true;  
 };
 
 function showBoard() {
     table.innerHTML = "";
     for (let i = 0; i < n; i++) {
         const tr = document.createElement("tr");
-        for (let j = 0; j < n; j++) {
+        for (let j = 0; j < m; j++) {
             const td = document.createElement("td");
-            td.innerText = getText(i, j);
+            td.innerText = "a";
             tr.appendChild(td);
         }
         table.appendChild(tr);
@@ -25,7 +23,7 @@ function showBoard() {
 
     
 
-const button = document.querySelector("button")
-button.addEventListener("click", start)
+const button = document.querySelector("button");
+button.addEventListener("click", startGame);
 
 
