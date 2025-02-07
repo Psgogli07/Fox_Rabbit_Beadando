ba9642d18cc108254da3090f1fa4ed0aa6c3f0d7
Kis Levente <kis.levente@ckik.loc>
2025-02-07 11:24:59 +0100

    LIURVA AFVAIO g

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ function randint(a, b) {
 function start(){
     showBoard()
     poziciok()
+    window.addEventListener("keyup", handleMove);
     button.disabled = true;  
     fox = { x: n - 1, y: m -1}; 
     rabbit = { x: n-1, y: m-1 };
@@ -34,11 +35,42 @@ function poziciok(){
     table.rows[4].cells[11].innerText = "🦊";
     let random1 = randint(0,n-1);
     let random2 = randint(0,m/2-1);  
-    console.log(random1);
-    console.log(random2);
     table.rows[random1].cells[random2].innerText = "🐰";
     
-}   
+} 
+
+function handleMove(e) {
+    let correctMove = true;
+    const previousTd = fox;
+    if (e.key === "ArrowUp" && fox.y > 0) {
+        fox.y--;
+    } else if (e.key === "ArrowDown" && fox.y < n-1) {
+        fox.y++;
+    } else if (e.key === "ArrowLeft" && fox.x > 0) {
+        fox.x--;
+    } else if (e.key === "ArrowRight" && fox.x < m-1) {
+        fox.x++;
+    } else {
+        correctMove = false;
+    }
+    if (correctMove) {
+        const currentTd = fox;
+        // currentTd.classList.add("current-position");
+        // previousTd.classList.remove("current-position");
+       currentTd.textContent = "🦊"
+        previousTd.textContent = "" 
+    }  
+}
+
+// function choosePosition(e) {
+//     const td = e.target;
+//     if (!td.matches("td")) return;
+//     fox.x = td.cellIndex;
+//     fox.y = td.parentNode.rowIndex;
+//     td.classList.add("current-position");
+//     window.addEventListener("keyup", handleMove);
+// }
+
 
 const button = document.querySelector("button")
 button.addEventListener("click", start)
